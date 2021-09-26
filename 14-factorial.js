00e87e61500608d01262703379be040b7cc4299b
gnzlplcs <palaciosgm@outlook.com>
2021-09-25 22:04:29 -0500

    factorial challenge completed

---
+++ 14-factorial.js
@@ -0,0 +1,19 @@
+/*
+Write a function factorial() that takes a number as an argument and returns the factorial of the number.
+
+Example:
+
+factorial(6);
+// returns `720` because 6 * 5 * 4 * 3 * 2 * 1 = 720
+Assume only positive numbers will be given as an argument for the factorial() function.
+*/
+
+const factorial = num => {
+  let result = 1;
+  for (let index = 1; index <= num; index++) {
+    result *= index;
+  }
+  return result;
+}
+
+console.log(factorial(5))
